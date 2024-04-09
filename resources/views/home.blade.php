b7ad2da5cd5dc95731497239e4f1240ced4ef1fc
PietroAntonioNini <151193470+PietroAntonioNini@users.noreply.github.com>
2024-04-09 14:43:17 +0200

    Esercizio completato + Bonus

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,23 @@
 @extends('layouts/app')
 
 @section('content')
-    <div class="container py5">
+<div class="my_container">
+    <div class="container py-5 text-center">
         <h1>Tutti i film del nostro database</h1>
-
-        <ul>
+        <div class="row pt-5">
             @foreach ($movies as $movie)
-            <li>
-                {{ $movie->title}}
-            </li>
+            <div class="col-sm-12 col-md-6 col-lg-4 col-xl-3 mb-4">
+                <div class="card">
+                    <div class="card-body">
+                        <h5 class="card-title">{{$movie->title}}</h5>
+                        <p class="card-text">{{$movie->original_title}}</p><br>
+                        <p class="card-text"><strong>Data:</strong> {{$movie->date}}</p>
+                        <p class="card-text"><strong>Voto:</strong> {{$movie->vote}}</p>
+                    </div>
+                </div>
+            </div>
             @endforeach
-        </ul>
+        </div>
     </div>
+</div>
 @endsection
